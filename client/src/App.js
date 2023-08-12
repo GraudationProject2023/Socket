b6adb86b69d5242af0f7eac1911ced51a5bfda8b
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-08-12 14:51:13 +0900

    client page init

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Main from "./Page/Main";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
